a365c772e740442415afac840a60d26dc244920f
Bryan Whyte <bwhyte21@outlook.com>
2021-10-01 13:42:34 -0400

    Add project files.

---
+++ AccountsTheGathering/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using AccountsTheGathering.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AccountsTheGathering.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController() { }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Error page that comes with default MVC project.
+        /// </summary>
+        /// <returns></returns>
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
